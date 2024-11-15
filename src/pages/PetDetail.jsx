5c7a2e3e0bcfc298e89ba6ec1b2ce875fbe1d94f
tamFEdev <tamnhtse151042@fpt.edu.vn>
2024-11-15 15:22:49 +0700

    update

--- src/pages/PetDetail.jsx
+++ src/pages/PetDetail.jsx
@@ -180,6 +180,7 @@ const PetDetail = () => {
       hasPetExperience: form.isAdoptPetBefore,
       reasonForAdopting: form.reason,
       note: "",
+      createDate: new Date().toISOString(),
     };
     try {
       const res = await axios.post(
